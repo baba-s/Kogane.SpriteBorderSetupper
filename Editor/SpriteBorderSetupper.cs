3bb86878a2e911cf42c00d710b1496f6cef6bd41
baba-s <shotababa1206@gmail.com>
2022-08-27 18:16:09 +0900

    update

--- Editor/SpriteBorderSetupper.cs
+++ Editor/SpriteBorderSetupper.cs
@@ -1,202 +1,202 @@
-﻿using OnionRing;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using OnionRing;
 using UnityEditor;
 using UnityEngine;
 
 namespace Kogane.Internal
 {
-	/// <summary>
-	/// スプライトの border を設定するクラス
-	/// </summary>
-	internal static class SpriteBorderSetupper
-	{
-		//================================================================================
-		// 定数
-		//================================================================================
-		private const string NAME           = "UniSpriteBorderSetupper";
-		private const string MENU_ITEM_NAME = "Assets/" + NAME + "/選択中のスプライトの Border を自動で設定";
+    /// <summary>
+    /// スプライトの border を設定するクラス
+    /// </summary>
+    internal static class SpriteBorderSetupper
+    {
+        //================================================================================
+        // 定数
+        //================================================================================
+        private const string NAME           = "SpriteBorderSetupper";
+        private const string MENU_ITEM_NAME = "Assets/Kogane/Setup Sprite Border";
 
-		//================================================================================
-		// デリゲート
-		//================================================================================
-		private delegate void DisplayProgressBarCallback
-		(
-			int    number,
-			int    count,
-			string path
-		);
+        //================================================================================
+        // デリゲート
+        //================================================================================
+        private delegate void DisplayProgressBarCallback
+        (
+            int    number,
+            int    count,
+            string path
+        );
 
-		private delegate void ClearProgressBarCallback();
+        private delegate void ClearProgressBarCallback();
 
-		//================================================================================
-		// クラス
-		//================================================================================
-		/// <summary>
-		/// テクスチャ情報を管理するクラス
-		/// </summary>
-		private sealed class TextureData
-		{
-			public string          Path     { get; }
-			public Texture2D       Texture  { get; }
-			public TextureImporter Importer { get; }
+        //================================================================================
+        // クラス
+        //================================================================================
+        /// <summary>
+        /// テクスチャ情報を管理するクラス
+        /// </summary>
+        private sealed class TextureData
+        {
+            public string          Path     { get; }
+            public Texture2D       Texture  { get; }
+            public TextureImporter Importer { get; }
 
-			public TextureData( Texture2D texture )
-			{
-				Path     = AssetDatabase.GetAssetPath( texture );
-				Texture  = texture;
-				Importer = AssetImporter.GetAtPath( Path ) as TextureImporter;
-			}
-		}
-		
-		//================================================================================
-		// 関数（static）
-		//================================================================================
-		/// <summary>
-		/// スプライトの border を設定します
-		/// </summary>
-		[MenuItem( MENU_ITEM_NAME )]
-		private static void DoSetup()
-		{
-			var isOk = EditorUtility.DisplayDialog
-			(
-				title: NAME,
-				message: "選択中のスプライトの Border を自動で設定しますか？",
-				ok: "OK",
-				cancel: "Cancel"
-			);
+            public TextureData( Texture2D texture )
+            {
+                Path     = AssetDatabase.GetAssetPath( texture );
+                Texture  = texture;
+                Importer = AssetImporter.GetAtPath( Path ) as TextureImporter;
+            }
+        }
 
-			if ( !isOk ) return;
+        //================================================================================
+        // 関数（static）
+        //================================================================================
+        /// <summary>
+        /// スプライトの border を設定します
+        /// </summary>
+        [MenuItem( MENU_ITEM_NAME )]
+        private static void DoSetup()
+        {
+            var isOk = EditorUtility.DisplayDialog
+            (
+                title: NAME,
+                message: "選択中のスプライトの Border を自動で設定しますか？",
+                ok: "OK",
+                cancel: "Cancel"
+            );
 
-			var textureListAtFile = Selection.objects
-					.OfType<Texture2D>()
-					.ToArray()
-				;
+            if ( !isOk ) return;
 
-			var allAssetPaths = AssetDatabase.GetAllAssetPaths();
+            var textureListAtFile = Selection.objects
+                    .OfType<Texture2D>()
+                    .ToArray()
+                ;
 
-			// フォルダが選択されている場合は
-			// そのフォルダ以下のすべてのテクスチャを対象にする
-			var textureListInFolder = Selection.objects
-					.Select( x => AssetDatabase.GetAssetPath( x ) )
-					.Where( x => AssetDatabase.IsValidFolder( x ) )
-					.SelectMany( x => allAssetPaths.Where( y => y.StartsWith( x ) ) )
-					.Select( x => AssetDatabase.LoadAssetAtPath<Texture2D>( x ) )
-					.Where( x => x != null )
-					.ToArray()
-				;
+            var allAssetPaths = AssetDatabase.GetAllAssetPaths();
 
-			var textureList = textureListAtFile
-					.Concat( textureListInFolder )
-					.Distinct()
-					.ToArray()
-				;
-			
-			if ( !textureList.Any() ) return;
+            // フォルダが選択されている場合は
+            // そのフォルダ以下のすべてのテクスチャを対象にする
+            var textureListInFolder = Selection.objects
+                    .Select( x => AssetDatabase.GetAssetPath( x ) )
+                    .Where( x => AssetDatabase.IsValidFolder( x ) )
+                    .SelectMany( x => allAssetPaths.Where( y => y.StartsWith( x ) ) )
+                    .Select( x => AssetDatabase.LoadAssetAtPath<Texture2D>( x ) )
+                    .Where( x => x != null )
+                    .ToArray()
+                ;
 
-			void OnDisplayProgressBarPreprocess( int number, int count, string path )
-			{
-				EditorUtility.DisplayProgressBar
-				(
-					title: $"{NAME} Preprocess",
-					info: $"{number}/{count} {path}",
-					progress: ( float ) number / count
-				);
-			}
+            var textureList = textureListAtFile
+                    .Concat( textureListInFolder )
+                    .Distinct()
+                    .ToArray()
+                ;
 
-			void OnDisplayProgressBarProcessing( int number, int count, string path )
-			{
-				EditorUtility.DisplayProgressBar
-				(
-					title: $"{NAME} Processing",
-					info: $"{number}/{count} {path}",
-					progress: ( float ) number / count
-				);
-			}
+            if ( !textureList.Any() ) return;
 
-			void OnComplete()
-			{
-				EditorUtility.DisplayDialog
-				(
-					title: NAME,
-					message: "選択中のスプライトの Border を自動で設定しました",
-					ok: "OK"
-				);
-			}
+            void OnDisplayProgressBarPreprocess( int number, int count, string path )
+            {
+                EditorUtility.DisplayProgressBar
+                (
+                    title: $"{NAME} Preprocess",
+                    info: $"{number}/{count} {path}",
+                    progress: ( float )number / count
+                );
+            }
 
-			Setup
-			(
-				textureList: textureList,
-				onDisplayProgressBarPreprocess: OnDisplayProgressBarPreprocess,
-				onDisplayProgressBarProcessing: OnDisplayProgressBarProcessing,
-				onClearProgressBar: EditorUtility.ClearProgressBar,
-				onComplete: OnComplete
-			);
-		}
+            void OnDisplayProgressBarProcessing( int number, int count, string path )
+            {
+                EditorUtility.DisplayProgressBar
+                (
+                    title: $"{NAME} Processing",
+                    info: $"{number}/{count} {path}",
+                    progress: ( float )number / count
+                );
+            }
 
-		private static void Setup
-		(
-			IEnumerable<Texture2D>     textureList,
-			DisplayProgressBarCallback onDisplayProgressBarPreprocess = default,
-			DisplayProgressBarCallback onDisplayProgressBarProcessing = default,
-			ClearProgressBarCallback   onClearProgressBar             = default,
-			Action                     onComplete                     = default
-		)
-		{
-			var list = textureList
-					.Select( c => new TextureData( c ) )
-					.ToArray()
-				;
+            void OnComplete()
+            {
+                EditorUtility.DisplayDialog
+                (
+                    title: NAME,
+                    message: "選択中のスプライトの Border を自動で設定しました",
+                    ok: "OK"
+                );
+            }
 
-			var count = list.Length;
+            Setup
+            (
+                textureList: textureList,
+                onDisplayProgressBarPreprocess: OnDisplayProgressBarPreprocess,
+                onDisplayProgressBarProcessing: OnDisplayProgressBarProcessing,
+                onClearProgressBar: EditorUtility.ClearProgressBar,
+                onComplete: OnComplete
+            );
+        }
 
-			try
-			{
-				AssetDatabase.StartAssetEditing();
+        private static void Setup
+        (
+            IEnumerable<Texture2D>     textureList,
+            DisplayProgressBarCallback onDisplayProgressBarPreprocess = default,
+            DisplayProgressBarCallback onDisplayProgressBarProcessing = default,
+            ClearProgressBarCallback   onClearProgressBar             = default,
+            Action                     onComplete                     = default
+        )
+        {
+            var list = textureList
+                    .Select( c => new TextureData( c ) )
+                    .ToArray()
+                ;
 
-				foreach ( var ( index, val ) in list.Select( ( val, index ) => ( index, val ) ) )
-				{
-					onDisplayProgressBarPreprocess?.Invoke( index + 1, count, val.Path );
+            var count = list.Length;
 
-					var importer = val.Importer;
-					importer.isReadable = true;
-					importer.SaveAndReimport();
-				}
-			}
-			finally
-			{
-				AssetDatabase.StopAssetEditing();
-				onClearProgressBar?.Invoke();
-			}
+            try
+            {
+                AssetDatabase.StartAssetEditing();
 
-			// isReadable を true にしてからじゃないと TextureSlicer.Slice が使用できないため
-			// isReadable を true にして 1 フレーム待機してから
-			// TextureSlicer.Slice を使用しています
-			EditorApplication.delayCall += () =>
-			{
-				try
-				{
-					AssetDatabase.StartAssetEditing();
+                foreach ( var (index, val) in list.Select( ( val, index ) => ( index, val ) ) )
+                {
+                    onDisplayProgressBarPreprocess?.Invoke( index + 1, count, val.Path );
 
-					foreach ( var ( index, val ) in list.Select( ( val, index ) => ( index, val ) ) )
-					{
-						onDisplayProgressBarProcessing?.Invoke( index + 1, count, val.Path );
+                    var importer = val.Importer;
+                    importer.isReadable = true;
+                    importer.SaveAndReimport();
+                }
+            }
+            finally
+            {
+                AssetDatabase.StopAssetEditing();
+                onClearProgressBar?.Invoke();
+            }
 
-						var slicedTexture = TextureSlicer.Slice( val.Texture );
-						var importer      = val.Importer;
-						importer.spriteBorder = slicedTexture.Boarder.ToVector4();
-						importer.isReadable   = false;
-						importer.SaveAndReimport();
-					}
-				}
-				finally
-				{
-					AssetDatabase.StopAssetEditing();
-					onClearProgressBar?.Invoke();
-					onComplete?.Invoke();
-				}
-			};
-		}
-	}
+            // isReadable を true にしてからじゃないと TextureSlicer.Slice が使用できないため
+            // isReadable を true にして 1 フレーム待機してから
+            // TextureSlicer.Slice を使用しています
+            EditorApplication.delayCall += () =>
+            {
+                try
+                {
+                    AssetDatabase.StartAssetEditing();
+
+                    foreach ( var (index, val) in list.Select( ( val, index ) => ( index, val ) ) )
+                    {
+                        onDisplayProgressBarProcessing?.Invoke( index + 1, count, val.Path );
+
+                        var slicedTexture = TextureSlicer.Slice( val.Texture );
+                        var importer      = val.Importer;
+                        importer.spriteBorder = slicedTexture.Boarder.ToVector4();
+                        importer.isReadable   = false;
+                        importer.SaveAndReimport();
+                    }
+                }
+                finally
+                {
+                    AssetDatabase.StopAssetEditing();
+                    onClearProgressBar?.Invoke();
+                    onComplete?.Invoke();
+                }
+            };
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Uni Sprite Border Setupper
+# Kogane Sprite Border Setupper
 
 スプライトの Border をコンテキストメニューから設定できるエディタ拡張
 
@@ -7,17 +7,17 @@
 ![2020-04-22_224039](https://user-images.githubusercontent.com/6134875/79989236-98439100-84ea-11ea-942d-05832aec5a0a.png)
 
 Border を設定したいスプライトを右クリックして「UniSpriteBorderSetupper >  
-選択中のスプライトの Border を自動で設定」を選択すると  
+選択中のスプライトの Border を自動で設定」を選択すると
 
 ![2020-04-22_224149](https://user-images.githubusercontent.com/6134875/79989232-97aafa80-84ea-11ea-8abe-d51f06f8f926.png)
 
-指定したスプライトの Border が自動で設定されます  
+指定したスプライトの Border が自動で設定されます
 
 ![2020-04-22_224203](https://user-images.githubusercontent.com/6134875/79989235-98439100-84ea-11ea-8648-9b7e5226ff0e.png)
 
-複数のスプライトを選択してコマンドを実行することもできます  
+複数のスプライトを選択してコマンドを実行することもできます
 
 ## 謝辞
 
-* 「UniSpriteBorderSetupper」は下記の OSS を使用させていただいております  
+* 「UniSpriteBorderSetupper」は下記の OSS を使用させていただいております
     * [OnionRing](https://github.com/kyubuns/OnionRingUnity)  
